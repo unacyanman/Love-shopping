d7b2bdd6de4875d8fe8b839bd30e8e1cd3d2d53c
unacyanman <166233150+unacyanman@users.noreply.github.com>
2024-08-12 07:20:35 +0000

    [Add]管理者機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+  
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
+  end
+
   get 'top' => 'homes#top'
   get 'homes/about', to: 'homes#about', as: :about
 
